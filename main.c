a4568a8c3b38471f1d4d155d651b1304258b3c55
Takosama_Rin <takosama.312@gmail.com>
2022-05-22 13:52:14 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-char getval(char* arr,int end, int n, int offset)
+char getval(char* arr, int end, int n, int offset)
 {
 	int idx = end - 1 - n;
 
 	if (idx % 2 == 0)
 		offset += 4;
-	
+
 	short val = (*(arr + idx / 2 - 1) * 256) | (255 & *(arr + idx / 2));
 	val >>= offset;
 	val &= 15;
 
-return val;
+	return val;
 }
 
 void setval(char* arr, int end, int n, int offset, char val)
@@ -35,10 +35,11 @@ void setval(char* arr, int end, int n, int offset, char val)
 	*(arr + idx / 2 - 1) = (*(arr + idx / 2 - 1) & ~mask1) | (tmp1 & mask1);
 }
 
-int main(void)
+char* _BCDtoBin_Converter_Read_DECtoBCD(FILE* input,int* cnt)
 {
 	int size = 1;
-	int cnt = 1;
+	 *cnt = 1;
+
 	char* p = malloc(size);
 	*p = NULL;
 	while (1)
@@ -47,12 +48,12 @@ int main(void)
 		if (c > '9' || c < '0')
 			break;
 
-		if (cnt % 2 == 0)
-			*(p + cnt / 2) = (c - '0') * 16;
+		if (*cnt % 2 == 0)
+			*(p + *cnt / 2) = (c - '0') * 16;
 		else
-			*(p + cnt / 2) += (c - '0');
+			*(p + *cnt / 2) += (c - '0');
 
-		cnt++;
+		(* cnt)++;
 		if (size * 2 <= cnt)
 		{
 			p = realloc(p, size * 2);
@@ -63,40 +64,80 @@ int main(void)
 			size *= 2;
 		}
 	}
+	return p;
+}
 
-	int iter = 0;
-	int pos = 0;
-	int end = 0;
-	char c = -1;
-	int i = 0;
-	int allzero = 0;
-	c = getval(p, cnt, end, iter);
+typedef struct _BCDtoBin_Converter_State
+{
+	int cnt;
+	int end;
+	int iter;
+	char* ptr;
+}BCDtoBin_Converter_State;
+
+typedef struct _BCDtoBin_Converter_Result
+{
+	int value;
+	int isend;
+}BCDtoBin_Converter_Result;
+
+
+void _BCDtoBin_Converter_Init(BCDtoBin_Converter_State* s, BCDtoBin_Converter_Result* r, char* BCD_array, int cnt)
+{
+	s->end = 0;
+	s->iter = 0;
+	s->cnt = cnt;
+	s->ptr = BCD_array;
+	r->value= 1 & getval(s->ptr, s->cnt, s->end, s->iter);
+	r->isend = 0;
+}
+
+
+void _BCDtoBin_Converter_ComputeNext(BCDtoBin_Converter_State* s, BCDtoBin_Converter_Result* r)
+{
+	s->iter++;
+	if (s->iter == 4)
+	{
+		s->iter = 0;
+		s->end++;
+	}
+
+	r->isend = 0;
+	char c = 0;
+	for (int i = s->cnt - 2; i >= s->end; i--)
+	{
+		c = getval(s->ptr, s->cnt, i, s->iter);
+		if (c >= 8)
+		{
+			c -= 3;
+			setval(s->ptr, s->cnt, i, s->iter, c);
+		}
+		r->isend |= c;
+	}
+	r->isend = !r->isend;
+	r->value = c&1;
+}
+
+
+struct  _BCDtoBin_Converter_State state;
+struct  _BCDtoBin_Converter_Result result;
+
+int main(void)
+{
+	int* pcnt = malloc(sizeof(int));
+	char* p= _BCDtoBin_Converter_Read_DECtoBCD(stdin, pcnt);
+
+	_BCDtoBin_Converter_Init(&state, &result, p, *pcnt);
+	putchar(result.value+'0');
+
 	while (1)
 	{
-		putchar((c & 1) + '0');
-
-		iter++;
-		if (iter == 4)
-		{
-			iter = 0;
-			end++;
-		}
-
-		allzero = 0;
-		for (i = cnt - 2; i >= end; i--)
-		{
-			c = getval(p, cnt, i, iter);
-			if (c >= 8)
-			{
-				c -= 3;
-				setval(p, cnt, i, iter, c);
-			}
-			allzero |= c;
-		}
-
-		if (!allzero)
+		_BCDtoBin_Converter_ComputeNext(&state, &result);
+		if (result.isend)
 			break;
+		putchar(result.value + '0');
 	}
+
 
 	putchar('\n');
 	return 0;
